45afbb6c058ba5aa89a1bfb9daeb8b306fa35f6c
dahye <>
2022-06-29 19:28:56 +0900

    📝 카카오 유저 정보 Dto 추가

---
+++ src/main/java/com/devit/devitcertificationservice/user/dto/KakaoUserInfo.java
@@ -0,0 +1,12 @@
+package com.devit.devitcertificationservice.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class KakaoUserInfo {
+    Long id;
+    String email;
+    String nickname;
+}
